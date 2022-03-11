27d71c0bb2230abbe09d17ccdc42e64af69d4bb5
cchumacero <cnchumacero@gmail.com>
2022-03-10 21:01:50 -0300

    Se le da formato al codigo

--- main.js
+++ main.js
@@ -1,39 +1,32 @@
 let pantalla = document.querySelector(".pantalla");
-let operacion = "", a, b = "";
-let huboCalculo = false;
-let operacionReciente = false;
-function cambiar(esto)
-{
+let operacion = "", a, b = "", huboCalculo = false, operacionReciente = false;
+
+function cambiar(esto) {
     operacionReciente = false;
-    if(huboCalculo)
-    {
+    if (huboCalculo) {
         huboCalculo = false;
         pantalla.value = "";
     }
-    
+
     pantalla.value = pantalla.value + esto.textContent;
     huboCalculo = false;
 }
 
-function operar(esto)
-{
-    if(operacion != "" && !operacionReciente)
-    {
+function operar(esto) {
+    if (operacion != "" && !operacionReciente) {
         calcular(esto);
     }
     huboCalculo = false;
     operacion = esto.textContent;
     a = pantalla.value;
-    if(!operacionReciente)
-    {
+    if (!operacionReciente) {
         pantalla.value = pantalla.value + esto.textContent;
         operacionReciente = true;
     }
-    else pantalla.value = (pantalla.value).substring(0,(pantalla.value).length - 1) + esto.textContent;
+    else pantalla.value = (pantalla.value).substring(0, (pantalla.value).length - 1) + esto.textContent;
 }
 
-function borrar(esto)
-{
+function borrar(esto) {
     pantalla.value = "";
     a = "";
     b = "";
@@ -41,61 +34,49 @@ function borrar(esto)
     huboCalculo = false;
 }
 
-function calcular(esto)
-{
+function calcular(esto) {
     operacionReciente = false;
-    if(!huboCalculo)
-    {
+    if (!huboCalculo) {
         huboCalculo = true;
         let bandera = false;
-        for(let i = 0; i < (pantalla.value).length; i++)
-        {
-            if(bandera)
-            {
+        for (let i = 0; i < (pantalla.value).length; i++) {
+            if (bandera) {
                 b += (pantalla.value)[i];
             }
 
-            if((pantalla.value)[i] == operacion)
+            if ((pantalla.value)[i] == operacion)
                 bandera = true;
-            
-            
+
+
         }
         let c = parseFloat(a), d = parseFloat(b);
-        if(operacion == "+")
-        {
+        if (operacion == "+") {
             pantalla.value = c + d;
         }
-        else if(operacion == "-")
-        {
+        else if (operacion == "-") {
             pantalla.value = c - d;
         }
-        else if(operacion == "/")
-        {
+        else if (operacion == "/") {
             pantalla.value = c / d;
         }
-        else if(operacion == "x")
-        {
+        else if (operacion == "x") {
             pantalla.value = c * d;
         }
         b = "";
         operacion = "";
     }
-    
+
 }
 
-function convertir(esto)
-{
-    if(operacion == "")
-    {
+function convertir(esto) {
+    if (operacion == "") {
         pantalla.value = parseFloat(pantalla.value) * (-1);
     }
 }
 
-function decimal(esto)
-{
-    if(pantalla.value == "" || (pantalla.value)[(pantalla.value).length - 1] != '.')
-    {
-        if(huboCalculo)
+function decimal(esto) {
+    if (pantalla.value == "" || (pantalla.value)[(pantalla.value).length - 1] != '.') {
+        if (huboCalculo)
             pantalla.value = esto.textContent;
         else pantalla.value = pantalla.value + esto.textContent;
         huboCalculo = false;
